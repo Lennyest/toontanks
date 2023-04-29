ed2baf5b43b042ef645fe75f081bbee36610b3aa
Lenny <hwilsgaard@gmail.com>
2023-04-29 20:47:03 +0200

    Created Tower actor and implemented visibility.

--- Tower.cpp
+++ Tower.cpp
@@ -2,4 +2,49 @@
 
 
 #include "Tower.h"
+#include "Tank.h"
+#include "Kismet/GameplayStatics.h"
 
+void ATower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (CheckRange())
+	{
+		RotateTurret(Tank->GetActorLocation());
+	}
+}
+
+void ATower::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+	GetWorldTimerManager().SetTimer(FireTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
+}
+
+bool ATower::CheckRange()
+{
+	if (!Tank) return false;
+
+	if (const float Distance = FVector::Distance(GetActorLocation(), Tank->GetActorLocation()); Distance > MaximumDistance) return false;
+
+	FHitResult Result;
+	const bool Hit = GetWorld()->LineTraceSingleByChannel(
+		Result,
+		ProjectileSpawnPoint->GetComponentLocation(),
+		Tank->GetActorLocation(),
+		ECC_Visibility
+	);
+	
+	return Hit && Result.GetActor() == UGameplayStatics::GetPlayerPawn(this, 0) && Result.ImpactPoint != FVector::Zero();
+}
+
+void ATower::CheckFireCondition()
+{
+	if (CheckRange())
+	{
+		Fire();
+	}
+}
--- Tower.h
+++ Tower.h
@@ -1,17 +1,35 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+/**
+ * 
+ */
 #pragma once
 
 #include "CoreMinimal.h"
 #include "BasePawn.h"
 #include "Tower.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class TOONTANKS_API ATower : public ABasePawn
 {
 	GENERATED_BODY()
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	virtual void BeginPlay() override;
+	bool CheckRange();
+
+private:
+	class ATank* Tank;
+
+	UPROPERTY(EditAnywhere)
+	float MaximumDistance = 800;
 	
+	UPROPERTY(EditAnywhere)
+	float FireRate = 2.0f;
+	
+	FTimerHandle FireTimerHandle;
+	
+	void CheckFireCondition();
 };
