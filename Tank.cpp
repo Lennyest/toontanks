0ead37bbfd55b283c7b965b552b2bd2c1a320718
Lenny <hwilsgaard@gmail.com>
2023-04-29 19:18:56 +0200

    Implemented basic tank & turret movement

--- Tank.cpp
+++ Tank.cpp
@@ -5,6 +5,7 @@
 
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 
 ATank::ATank()
@@ -19,9 +20,72 @@ ATank::ATank()
 void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
+	PlayerInputComponent->BindAxis(TEXT("RotateTurret"), this, &ATank::Rotate);
 }
 
-void ATank::Move(float Value)
+void ATank::Tick(float DeltaTime)
 {
+	Super::Tick(DeltaTime);
+
+	if (PlayerControllerRef)
+	{
+		FHitResult Result;
+		PlayerControllerRef->GetHitResultUnderCursor(ECC_Visibility, false, Result);
+
+		DrawDebugSphere(GetWorld(), Result.ImpactPoint, 10, 6, FColor::Red, false, 1);
+	}
+}
+
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PlayerControllerRef = Cast<APlayerController>(GetController());
+}
+
+void ATank::RotateTurret(FVector TargetPosition)
+{
+	// Do not handle rotation if our result is 0!
+	if (TargetPosition == FVector::Zero()) return;
 	
+	const FRotator TargetRotation = (GetActorLocation() - TargetPosition).Rotation();
+	const FRotator TurretRotation = FRotator(0, TargetRotation.Yaw + 180, 0);
+
+	LastRotation = TurretRotation;
+	TurretMesh->SetWorldRotation(
+		FMath::RInterpTo(
+			TurretMesh->GetComponentRotation(),
+			TurretRotation,
+			UGameplayStatics::GetWorldDeltaSeconds(this),
+			RotationSpeed)
+		);
+}
+
+void ATank::Move(const float Value)
+{
+	FVector DeltaLocation(0.f);
+	DeltaLocation.X = Value * Speed * UGameplayStatics::GetWorldDeltaSeconds(this);
+	
+	AddActorLocalOffset(DeltaLocation, true);
+}
+
+void ATank::Turn(const float Value)
+{
+	FRotator DeltaRotation(0.);
+	DeltaRotation.Yaw = Value * TurnSpeed * UGameplayStatics::GetWorldDeltaSeconds(this);
+
+	AddActorLocalRotation(DeltaRotation, true);
+}
+
+void ATank::Rotate(float Value)
+{
+	if (!PlayerControllerRef) return;
+	
+	FHitResult Result;
+	PlayerControllerRef->GetHitResultUnderCursor(ECC_Visibility, false, Result);
+
+	RotateTurret(Result.ImpactPoint);
 }
--- Tank.h
+++ Tank.h
@@ -20,8 +20,24 @@ class TOONTANKS_API ATank : public ABasePawn
 
 public:
 	ATank();
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Speed = 400.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float TurnSpeed = 50.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float RotationSpeed = 25.0f;
+
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	virtual void BeginPlay() override;
+	void RotateTurret(FVector TargetPosition);
 
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
@@ -30,5 +46,12 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta=(AllowPrivateAccess="true"))
 	USpringArmComponent* SpringArm;
 
+	// Value is direction provided by Axis mapping.
 	void Move(float Value);
+	void Turn(float Value);
+	void Rotate(float Value);
+
+	// Storing our last rotation if our result is null.
+	FRotator LastRotation;
+	APlayerController* PlayerControllerRef;
 };
