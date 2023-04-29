82547996d991147518c48ee50fc3bc12f8bd593d
Lenny <hwilsgaard@gmail.com>
2023-04-29 20:46:14 +0200

    Moved Rotate turret to BasePawn and created Fire method (TBD)

--- BasePawn.cpp
+++ BasePawn.cpp
@@ -4,6 +4,7 @@
 #include "BasePawn.h"
 
 #include "Components/CapsuleComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -22,4 +23,27 @@ ABasePawn::ABasePawn()
 
 	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Projectile Spawn Point"));
 	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
+}
+
+void ABasePawn::RotateTurret(const FVector &TargetPosition)
+{
+	// Do not handle rotation if our result is 0!
+	if (TargetPosition == FVector::Zero()) return;
+	
+	const FRotator TargetRotation = (GetActorLocation() - TargetPosition).Rotation();
+	const FRotator TurretRotation = FRotator(0, TargetRotation.Yaw + 180, 0);
+
+	LastRotation = TurretRotation;
+	TurretMesh->SetWorldRotation(
+		FMath::RInterpTo(
+			TurretMesh->GetComponentRotation(),
+			TurretRotation,
+			UGameplayStatics::GetWorldDeltaSeconds(this),
+			RotationSpeed)
+		);
+}
+
+void ABasePawn::Fire()
+{
+	DrawDebugSphere(GetWorld(), ProjectileSpawnPoint->GetComponentLocation(), 10, 6, FColor::Silver, false, 3);
 }
--- BasePawn.h
+++ BasePawn.h
@@ -17,11 +17,21 @@ public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float RotationSpeed = 25.0f;
+	
 protected:
 	// Called when the game starts or when spawned
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Tank", meta=(AllowPrivateAccess="true"))
 	UStaticMeshComponent* TurretMesh;
 
+	void RotateTurret(const FVector& TargetPosition);
+	void Fire();
+	
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Tank", meta=(AllowPrivateAccess="true"))
+	USceneComponent* ProjectileSpawnPoint;
+	
 private:
 	// The use of the allowprivateaccess meta tag is silly, ideally you would place these in the public section instead...
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Tank", meta=(AllowPrivateAccess="true"))
@@ -30,7 +40,5 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Tank", meta=(AllowPrivateAccess="true"))
 	UStaticMeshComponent* BaseMesh;
 	
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Tank", meta=(AllowPrivateAccess="true"))
-	USceneComponent* ProjectileSpawnPoint;
+	FRotator LastRotation;
 };
